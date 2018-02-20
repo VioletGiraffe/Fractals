08abf3028017e2bcda040269e6c8eecbeb573f8c
VioletGiraffe <alexxxx89@ya.ru>
2018-02-20 23:39:59 +0200

    OpenCL something added

---
+++ ocl/openclproxy.cpp
@@ -0,0 +1,6 @@
+#include "openclproxy.h"
+
+OpenClProxy::OpenClProxy()
+{
+
+}
---
+++ ocl/openclproxy.h
@@ -0,0 +1,11 @@
+#ifndef OPENCLPROXY_H
+#define OPENCLPROXY_H
+
+
+class OpenClProxy
+{
+public:
+	OpenClProxy();
+};
+
+#endif // OPENCLPROXY_H
